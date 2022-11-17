2815291e520556eb65e78c42bd5398f11b1207e2
gaabrieljesuss <gjs8@aluno.ifal.edu.br>
2022-11-17 15:51:11 -0500

    Add initial project

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by user230124 on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
